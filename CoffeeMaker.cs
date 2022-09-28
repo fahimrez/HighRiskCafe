0b0ec9c3e5c0b4fc4be45b7ea39feec2e6cfd3a6
fahimrez <113334759+fahimrez@users.noreply.github.com>
2022-09-28 09:57:39 +0200

    Update CoffeeMaker.cs

--- CoffeeMaker.cs
+++ CoffeeMaker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,15 +10,17 @@ namespace HighRiskCafe
     internal class CoffeeMaker
     {
         private List<string> coffeeTypes = new()
+
         {
-            "Espresso",
-            "Americano",
-            "Macchiato",
-            "Cortado",
-            "Cappucino",
-            "Mocha"
+            "espresso",
+            "americano",
+            "macchiato",
+            "cortado",
+            "cappucino",
+            "mocha"
         };
 
+        private Coffee coffee;
         public void MakeCoffee()
         {
             //Ask if user wants a coffee
@@ -27,7 +30,201 @@ namespace HighRiskCafe
             {
                 //Display coffee types
                 DisplayCoffeeTypes();
+
+                // Ask what coffee type the user wants
+                string validCoffeeType = AskCoffeeType();
+
+                // Create a coffee object
+                CreateCoffee(validCoffeeType);
+
+                // Attempt to put coffee in machine
+                bool success = PutCoffeeInMachine();
+
+                if (success)
+                {
+                    success = PutWaterInMachine();
+
+                    if (success)
+                    {
+                        success = PlaceCupInMachine();
+
+                        if (success)
+                        {
+                            success = StartMachine();
+
+                            if (success)
+                            {
+                                success = ServeCoffeeToGuests();
+
+                                if (success) 
+                                {
+                                    coffee.IsDone = true;
+                                }
+                            }
+                        }
+                    }
+                }
+
+                DisplayResult();
             }
+        }
+
+        private void DisplayResult()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Coffee: ");
+            Console.WriteLine($"Is done: {coffee.IsDone}");
+            Console.WriteLine($"Coffee type: {coffee.CoffeeType}");
+            Console.WriteLine($"Enjoy your {coffee.CoffeeType}");
+            if (!coffee.IsDone)
+            {
+                Console.WriteLine($"Failed step: {coffee.FailStep}");
+            }
+        }
+
+        private bool ServeCoffeeToGuests()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Attempting to serve coffee...");
+
+            bool success = DetermineSuccess();
+
+            if (success)
+            {
+
+                return true;
+            }
+            else
+            {
+                coffee.FailStep = "Failed to serve coffee...";
+                return false;
+            }
+
+        }
+
+        private bool StartMachine()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Attempting to start machine...");
+
+            bool success = DetermineSuccess();
+
+            if (success)
+            {
+                return true;
+            }
+            else
+            {
+                coffee.FailStep = "Failed to start the machine...";
+                return false;
+            }
+
+        }
+
+        private bool PlaceCupInMachine()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Attempting to put the cup in the machine...");
+
+            bool success = DetermineSuccess();
+
+            if (success)
+            {
+                return true;
+            }
+            else
+            {
+                coffee.FailStep = "Failed to put the the cup in the machine...";
+                return false;
+            }
+
+        }
+
+        private bool PutWaterInMachine()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Attempting to put water in machine...");
+
+            bool success = DetermineSuccess();
+
+            if (success)
+            {
+                return true;
+            }
+            else
+            {
+                coffee.FailStep = "Failed to put the water in the machine...";
+                return false;
+            }
+
+        }
+
+        private bool PutCoffeeInMachine()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Attempting to put coffee in machine...");
+
+            bool success = DetermineSuccess();
+
+            if (success)
+            {
+                return true;
+            }
+            else
+            {
+                coffee.FailStep = "Failed to put the coffee in the machine...";
+                return false;
+            }
+          
+        }
+
+        private bool DetermineSuccess()
+        {
+            int randomNumber =  new Random().Next(0,100);
+
+            if(randomNumber >= 20)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void CreateCoffee(string validCoffeeType)
+        {
+            coffee = new();
+            coffee.IsDone = false;
+            coffee.CoffeeType = validCoffeeType;
+            coffee.FailStep = "";
+        }
+
+        private string AskCoffeeType()
+        {
+            Console.WriteLine();
+            Console.WriteLine("What coffee type do you want?");
+            Console.WriteLine("Answer: ");
+
+            string answer = Console.ReadLine();
+
+            bool isValidCoffeeType = ValidateCoffeeType(answer);
+
+            if (isValidCoffeeType)
+            {
+                return answer;
+            }
+
+            AskCoffeeType();
+            return "";
+        }
+
+        private bool ValidateCoffeeType(string? answer)
+        {
+            if (coffeeTypes.Contains(answer.ToLower()))
+            {
+                return true;
+            }
+
+            return false;
         }
 
         private void DisplayCoffeeTypes()
